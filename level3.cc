13ccc977318d89eb9e50d35622b9f22ec0e40ee8
Sahil Pahooja <pahoojasahil@gmail.com>
2019-11-21 13:43:45 -0500

    Implementing level 3 design

---
+++ level3.cc
@@ -0,0 +1,33 @@
+#include "level3.h"
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+LevelThree::LevelThree(int s): seed{s} {}
+
+Piece* LevelThree::nextPiece(vector<vector<Cell*>> &t) {
+	srand(this->seed);
+	int pos = rand()%9+1;
+    Piece *pc = nullptr;
+    if ((pos==1)||(pos==2)) {
+        pc = new PieceZ(t);
+    }
+    else if ((pos==3)||(pos==4)) {
+        pc = new PieceS(t);
+    }
+    else if (pos==5) {
+        pc = new PieceI(t);
+    }
+    else if (pos==6) {
+        pc = new PieceO(t);
+    }
+    else if (pos==7) {
+        pc = new PieceT(t);
+    }
+    else if (pos==8) {
+        pc = new PieceJ(t);
+    }
+    else if (pos==9) {
+    }
+    return pc;
+    }
---
+++ level3.h
@@ -0,0 +1,22 @@
+#ifndef LEVEL3_H
+#define LEVEL3_H
+#include "level.h"
+#include "pieceI.h"
+#include "pieceZ.h"
+#include "pieceS.h"
+#include "pieceT.h"
+#include "pieceO.h"
+#include "pieceJ.h"
+#include <fstream>
+
+class Piece;
+class Cell;
+
+class LevelThree: public Level {
+	int seed;
+	public:
+		LevelThree(int s=0);
+		Piece* nextPiece(std::vector<std::vector<Cell*>> &t) override;
+};
+
+#endif
