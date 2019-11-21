bb19f83633672e0252a726613b1186f4fb9102be
Sahil Pahooja <pahoojasahil@gmail.com>
2019-11-21 14:12:31 -0500

    Implement level 4 design

--- level1.d
+++ level1.d
@@ -1,2 +1,2 @@
 level1.o: level1.cc level1.h level.h piece.h cell.h pieceI.h pieceZ.h \
- pieceS.h pieceT.h pieceO.h
+ pieceS.h pieceT.h pieceO.h pieceJ.h
---
+++ level2.d
@@ -0,0 +1,2 @@
+level2.o: level2.cc level2.h level.h piece.h cell.h pieceI.h pieceZ.h \
+ pieceS.h pieceT.h pieceO.h pieceJ.h
